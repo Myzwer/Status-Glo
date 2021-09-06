ad50dabb354cffd65e0939db06a38e57ab0d7208
Myzwer <josh@onefortyfivedesign.com>
2021-09-06 00:14:20 -0400

    :rocket: Hero and first card section of the frontpage done. Honestly way too big of a commit, but here we are.

--- frontpage.php
+++ frontpage.php
@@ -12,65 +12,78 @@
  */
 
 get_header(); ?>
-
-    <video class="header-video" src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
-
-    <div class="viewport-header">
-        <div class="head-container">
-            <div class="center add-padding">
-                <h1 class="text-white text-5xl pb-5">Header Title</h1>
-            </div>
-            <hr class="text-white pb-5">
-            <h2 class="text-white text-3xl ">Title</h2>
-            <h3 class="text-white text-2xl">Subtitle</h3>
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+  rgba(0, 150, 238, 0.25),
+  rgba(0, 68, 216, 0.25)
+), url('http://status-glo.local/wp-content/uploads/2021/09/Frontpage-Header-2.jpg') center center;
+ height: 60vh;">
+        <div class="content-middle text-black text-center">
+            <h1 class="text-6xl md:text-7xl mb-3">Status Glo</h1>
+            <hr class="text-blue-dark w-2/4 m-auto">
+            <h2 class="text-xl md:text-3xl my-5 mx-auto leading-5 md:w-2/5 md:max-w-lg">Cleaning services for homes and
+                offices by serving them with care and love. </h2>
+            <a href=""
+               class="bg-orange rounded-full font-bold shadow-xl text-black px-8 py-3 transition duration-300 ease-in-out hover:bg-orange-hover mt-10">
+                Book Your Cleaning
+            </a>
         </div>
     </div>
 
     <div class="bg-white pb-10">
-        <div class="m-4 md:m-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-            <div class="grid">
-                <div class=" grid-cols-12">
-                    <div class="col-span-12">
-                        <div class="text-center md:text-left mb-1">
-                            <h1>Color Pod</h1>
-                            <p>All fields optional</p>
+        <div class="mx-4 md:mx-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
+            <div class="col-span-12 text-center my-5">
+                <h2 class="text-5xl">Services</h2>
+            </div>
+
+            <div class="grid grid-cols-12">
+                <div class="col-span-12 card-gradient-1 rounded-xl shadow-2xl">
+                    <div class="text-center p-4">
+                        <h2 class="text-3xl">Book Your Cleaning</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
+                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
+                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
+                        <div class="my-5">
+                            <a href=""
+                               class="bg-orange rounded-full font-bold shadow-xl text-black px-8 py-3 transition duration-300 ease-in-out hover:bg-orange-hover">
+                                Book Your Cleaning
+                            </a>
                         </div>
-                        <hr>
                     </div>
                 </div>
             </div>
 
+
             <div class="grid grid-cols-12 gap-4 mt-6">
-                <div class="col-span-12 lg:col-span-4 card-gradient-1 rounded-xl shadow-xl">
+                <div class="col-span-12 text-center mt-10">
+                    <h2 class="text-4xl">Cleaning for every situation</h2>
+                </div>
+                <div class="col-span-12 md:col-span-6 card-gradient-1 rounded-xl shadow-2xl">
                     <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
+                        <h2 class="text-3xl">Book Your Cleaning</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
+                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
+                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
+                        <div class="my-5">
+                            <a href=""
+                               class="bg-orange rounded-full font-bold shadow-xl text-black px-8 py-3 transition duration-300 ease-in-out hover:bg-orange-hover">
+                                Book Your Cleaning
+                            </a>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-2 rounded-xl shadow-xl">
+                <div class="col-span-12 md:col-span-6 card-gradient-1 rounded-xl shadow-2xl">
                     <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-3 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class = "font-bold text-2xl">Title</h2>
-                        <h4 class = "font-semibold">Subtitle</h4>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
+                        <h2 class="text-3xl">Book Your Cleaning</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
+                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
+                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
+                        <div class="my-5">
+                            <a href=""
+                               class="bg-orange rounded-full font-bold shadow-xl text-black px-8 py-3 transition duration-300 ease-in-out hover:bg-orange-hover">
+                                Book Your Cleaning
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -90,40 +103,64 @@ get_header(); ?>
                 <div class="col-span-12 md:col-span-6 lg:col-span-3">
                     <div class="text-center mb-1">
                         <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                            <img class="rounded-full"
+                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+                                 alt="">
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+                        <h2 class="font-bold text-2xl">PDF Title</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                            corporis
+                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla
+                            odio
+                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
                     </div>
                 </div>
 
                 <div class="col-span-12 md:col-span-6 lg:col-span-3">
                     <div class="text-center mb-1">
                         <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                            <img class="rounded-full"
+                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+                                 alt="">
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+                        <h2 class="font-bold text-2xl">PDF Title</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                            corporis
+                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla
+                            odio
+                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
                     </div>
                 </div>
 
                 <div class="col-span-12 md:col-span-6 lg:col-span-3">
                     <div class="text-center mb-1">
                         <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                            <img class="rounded-full"
+                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+                                 alt="">
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+                        <h2 class="font-bold text-2xl">PDF Title</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                            corporis
+                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla
+                            odio
+                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
                     </div>
                 </div>
 
                 <div class="col-span-12 md:col-span-6 lg:col-span-3">
                     <div class="text-center mb-1">
                         <div class="w-24 text-center m-auto my-3">
-                            <img class = "rounded-full" src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="">
+                            <img class="rounded-full"
+                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+                                 alt="">
                         </div>
-                        <h2 class = "font-bold text-2xl">PDF Title</h2>
-                        <p class = "text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+                        <h2 class="font-bold text-2xl">PDF Title</h2>
+                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                            corporis
+                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla
+                            odio
+                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
                     </div>
                 </div>
             </div>
