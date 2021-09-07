31f9092c0742e5bb2b6dc0a267b79ee2d44fe626
Myzwer <josh@onefortyfivedesign.com>
2021-09-07 01:08:55 -0400

    :scroll: Updated the headers on all main PHP pages.

---
+++ services.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Booking
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Status_glo
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+<div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+  rgba(0, 150, 238, 0.45),
+  rgba(0, 68, 216, 0.45)
+), url('http://status-glo.local/wp-content/uploads/2021/09/Contact-Header.jpg') center center;
+ height: 40vh;">
+    <div class="content-middle text-white text-center">
+        <h1 class="text-6xl md:text-7xl mb-3">Contact</h1>
+    </div>
+</div>
