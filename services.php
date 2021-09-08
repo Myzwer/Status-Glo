1f4dddd11019dd6a9f1797c9183777239441e44a
Myzwer <josh@onefortyfivedesign.com>
2021-09-08 00:56:32 -0400

    :construction: Finished the cards on the services page. It will need adjusted when converted to ACF, but the first one is correct.

--- services.php
+++ services.php
@@ -28,16 +28,116 @@ get_header(); ?>
 </div>
 
 <div class="bg-white pb-10">
-    <div class="mx-4 md:mx-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
+    <div class="mx-4 md:mx-10 lg:max-w-6xl lg:text-center lg:mx-auto pt-10">
         <div class="col-span-12 text-center my-5">
             <h2 class="text-5xl">Monthly Packages</h2>
         </div>
 
         <div class="grid grid-cols-12 gap-4 mt-6">
-            <div class="col-span-12 md:col-span-4 card-gradient-1 rounded-xl shadow-2xl text-white">
+            <div class="col-span-12 md:col-span-6 lg:col-span-3 card-gradient-1 rounded-xl shadow-2xl text-white">
+                <div class="rounded-t-xl py-7">
+                </div>
                 <div class="text-center p-5">
                     <h2 class="text-3xl text-left">Basic</h2>
-                    <p class="text-left leading-5">Basic package is designed to cover the most important cleaning needs such as
+                    <p class="text-left leading-5">Basic package is designed to cover the most important cleaning needs
+                        such as
+                        dusting and cleaning floors. This package will benefit if you need maintain the basic
+                        cleanliness of your home on a regular basis.</p>
+                    <div class="my-5">
+                        <a href=""
+                           class="rounded-full font-bold block border-2 border-white text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-blue-light">
+                            Book Now
+                        </a>
+                    </div>
+
+                    <div class="py-2 text-left">
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> 1,500 to 3,000 sq ft</p>
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> Flexible Pricing</p>
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> Covers Kitchen and Bathrooms</p>
+                    </div>
+
+                    <hr class="text-black m-auto">
+
+                    <div class="py-2 text-left">
+                        <p>+ Dusting</p>
+                        <p>+ Vacuuming</p>
+                        <p>+ Cleaning Floors</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-12 md:col-span-6 lg:col-span-3 card-gradient-1 rounded-xl shadow-2xl text-white">
+                <div class="bg-orange rounded-t-xl py-3">
+                    <h3 class="text-xl text-center">Most Popular!</h3>
+                </div>
+                <div class="text-center p-5">
+                    <h2 class="text-3xl text-left">Basic</h2>
+                    <p class="text-left leading-5">Basic package is designed to cover the most important cleaning needs
+                        such as
+                        dusting and cleaning floors. This package will benefit if you need maintain the basic
+                        cleanliness of your home on a regular basis.</p>
+                    <div class="my-8">
+                        <a href=""
+                           class="rounded-full font-bold border-2 border-white text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-blue-light">
+                            Book Now
+                        </a>
+                    </div>
+
+                    <div class="py-2 text-left">
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> 1,500 to 3,000 sq ft</p>
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> Flexible Pricing</p>
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> Covers Kitchen and Bathrooms</p>
+                    </div>
+
+                    <hr class="text-black m-auto">
+
+                    <div class="py-2 text-left">
+                        <p>+ Dusting</p>
+                        <p>+ Vacuuming</p>
+                        <p>+ Cleaning Floors</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-12 md:col-span-6 lg:col-span-3 card-gradient-1 rounded-xl shadow-2xl text-white">
+                <div class="rounded-t-xl py-7">
+                </div>
+                <div class="text-center p-5">
+                    <h2 class="text-3xl text-left">Basic</h2>
+                    <p class="text-left leading-5">Basic package is designed to cover the most important cleaning needs
+                        such as
+                        dusting and cleaning floors. This package will benefit if you need maintain the basic
+                        cleanliness of your home on a regular basis.</p>
+                    <div class="my-8">
+                        <a href=""
+                           class="rounded-full font-bold border-2 border-white text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-blue-light">
+                            Book Now
+                        </a>
+                    </div>
+
+                    <div class="py-2 text-left">
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> 1,500 to 3,000 sq ft</p>
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> Flexible Pricing</p>
+                        <p><i class="fas fa-check-circle text-blue-dark"></i> Covers Kitchen and Bathrooms</p>
+                    </div>
+
+                    <hr class="text-black m-auto">
+
+                    <div class="py-2 text-left">
+                        <p>+ Dusting</p>
+                        <p>+ Vacuuming</p>
+                        <p>+ Cleaning Floors</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-12 md:col-span-6 lg:col-span-3 card-gradient-1 rounded-xl shadow-2xl text-white">
+                <div class="rounded-t-xl py-7">
+                </div>
+                <div class="text-center p-5">
+                    <h2 class="text-3xl text-left">Basic</h2>
+                    <p class="text-left leading-5">Basic package is designed to cover the most important cleaning needs
+                        such as
                         dusting and cleaning floors. This package will benefit if you need maintain the basic
                         cleanliness of your home on a regular basis.</p>
                     <div class="my-8">
