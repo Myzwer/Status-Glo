d2eac697b15d414c9ad58b45bd2fedf6f092a632
Myzwer <josh@onefortyfivedesign.com>
2021-09-11 04:36:48 -0400

    :rocket: Converted the whole page to ACF. All text and images are now generated from WP backend, not hardcoded.

--- booking.php
+++ booking.php
@@ -11,23 +11,31 @@
  */
 
 get_header(); ?>
-<div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-  rgba(0, 150, 238, 0.45),
-  rgba(0, 68, 216, 0.45)
-), url('http://status-glo.local/wp-content/uploads/2021/09/Booking-Header.jpg') center center;
- height: 40vh;">
-    <div class="content-middle text-white text-center">
-        <h1 class="text-6xl md:text-7xl mb-3">Booking</h1>
-    </div>
-</div>
+<?php if (have_rows('header')): ?>
+    <?php while (have_rows('header')): the_row(); ?>
+        <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+                rgba(0, 150, 238, 0.<?php the_sub_field('tinting'); ?>),
+                rgba(0, 68, 216, 0.<?php the_sub_field('tinting'); ?>)
+                ), url('<?php the_sub_field('background_image'); ?>') <?php the_sub_field('v_position'); ?> <?php the_sub_field('h_position'); ?>;
+                height: 40vh;">
+            <div class="content-middle text-white text-center">
+                <h1 class="text-6xl md:text-7xl mb-3"><?php the_sub_field('main_title'); ?></h1>
+            </div>
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
 
 <div class="bg-white pb-10">
     <div class="mx-4 md:mx-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
         <div class="grid grid-cols-12">
             <div class="col-span-12 card-gradient-1 rounded-xl shadow-2xl mb-10">
                 <div class="text-center p-5 md:p-10 text-white">
-                    <h2 class="text-3xl text-left">Want to get booked?</h2>
-                    <p class="text-left">We offer recurring clean, deep or detail clean, custom clean, packages, One-time and customized clean. Our professional cleaning technicians are experienced and trustworthy and will take care of any job with excellence. We want our clients to be happy and satisfied! Contact us for more information.</p>
+                    <?php if (have_rows('button_card')): ?>
+                    <?php while (have_rows('button_card')): the_row(); ?>
+                    <h2 class="text-3xl text-left"><?php the_sub_field('title'); ?></h2>
+                            <p class="text-left"><?php the_sub_field('body_text'); ?></p>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
             </div>
 
@@ -44,3 +52,6 @@ get_header(); ?>
         </div>
     </div>
 </div>
+
+<?php
+get_footer();
