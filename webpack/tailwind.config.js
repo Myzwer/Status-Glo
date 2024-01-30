c19115836cf47332a0d0302d5178b846a791bd8c
Myzwer <josh@onefortyfivedesign.com>
2024-01-30 00:32:09 -0500

    :arrow_up: Updated tailwind to the latest version.

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -1,24 +1,29 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   /*
-   * Enable JIT mode, a bleeding edge experimental feature released with tailwind v2.1
-   * If you don't want to use this, just delete the line of code.
+   * This project now uses v3 of Tailwind.
+   * If you're familiar with the way a previous version worked, Tailwind no longer purges CSS, rather it uses a
+   * new feature called JIT (just in time) to call the CSS as needed as opposed to purging after the fact.
    *
-   * https://tailwindcss.com/docs/just-in-time-mode#enabling-jit-mode
+   * More information: https://tailwindcss.com/blog/tailwindcss-v3
    */
-  mode: "jit",
-
-  /*
-   * So the purge feature. Super cool.
-   * Below you need to set the purge location. This basically means where Tailwind needs to look for classes.
-   * It might seem backwards, but its checking those files for things NOT to purge.
-   * Unless you need to change it, you should be able to just leave this alone. It checks the php and js files.
-   *
-   * JIT MODE UPDATE: Does the same thing, but its instead looking for classes to ADD, not purge.
-   */
-  purge: ["*.php", "./assets/src/js/*.js"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["*.php", "./components/**/*.php", "./assets/src/js/*.js"],
   theme: {
     colors: {
+      screens: {
+        sm: "39.9375em",
+        // => @media (min-width: 640px) { ... }
+
+        md: "63.9375em",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "64em",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "74.9375em",
+        // => @media (min-width: 1280px) { ... }
+      },
       transparent: "transparent",
       current: "currentColor",
       /* How to Add custom Colors
@@ -48,8 +53,5 @@ module.exports = {
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
